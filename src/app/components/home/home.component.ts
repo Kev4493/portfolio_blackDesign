c1b5f69b7dc9d2307c957a6284dc17095e4a7ce0
Kevin Wagner <kevin_wagner4493@web.de>
2024-01-18 21:50:44 +0100

    added new projectpictures

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from "../skills/skills.component";
+import { ProjectsComponent } from "../projects/projects.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SkillsComponent]
+    imports: [SkillsComponent, ProjectsComponent]
 })
 export class HomeComponent {
 
