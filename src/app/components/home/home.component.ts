bf5c21b33263035dd77c896f4182b3f50ce11865
Kevin Wagner <kevin_wagner4493@web.de>
2024-02-02 22:23:35 +0100

    add new section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from "../skills/skills.component";
 import { ProjectsComponent } from "../projects/projects.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SkillsComponent, ProjectsComponent]
+    imports: [SkillsComponent, ProjectsComponent, AboutComponent]
 })
 export class HomeComponent {
 
