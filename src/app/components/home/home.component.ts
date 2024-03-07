bdfd2bbd5adf8c804af2422c1fe7f4352d2acc05
Kevin Wagner <kevin_wagner4493@web.de>
2024-03-07 22:11:16 +0100

    add contact-section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { SkillsComponent } from "../skills/skills.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { AboutComponent } from "../about/about.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SkillsComponent, ProjectsComponent, AboutComponent]
+    imports: [SkillsComponent, ProjectsComponent, AboutComponent, ContactComponent]
 })
 export class HomeComponent {
 
