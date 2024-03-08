ad9ad21588fdbad6d22c8f302cc7d95ebbc303e3
Kevin Wagner <kevin_wagner4493@web.de>
2024-03-08 22:15:13 +0100

    add footer section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,14 @@ import { SkillsComponent } from "../skills/skills.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { AboutComponent } from "../about/about.component";
 import { ContactComponent } from "../contact/contact.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [SkillsComponent, ProjectsComponent, AboutComponent, ContactComponent]
+    imports: [SkillsComponent, ProjectsComponent, AboutComponent, ContactComponent, FooterComponent]
 })
 export class HomeComponent {
 
