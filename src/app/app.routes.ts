092398c4a3c7713a3decc87e3b1a917acc4fa7d1
Kevin Wagner <kevin_wagner4493@web.de>
2024-03-15 22:59:59 +0100

    add checkout section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LegalNoticeComponent } from './components/legal-notice/legal-notice.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'imprint', component: LegalNoticeComponent },
+    { path: 'checkout', component: CheckoutComponent },
+
 ];
