156186be69de06301bf05f85d19d5ba9ba988cac
Kevin Wagner <kevin_wagner4493@web.de>
2024-03-18 16:56:37 +0100

    add typing effect to home section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,4 +14,42 @@ import { FooterComponent } from "../footer/footer.component";
 })
 export class HomeComponent {
 
+    constructor() { }
+
+    ngAfterViewInit(): void {
+        const dynamicText = document.querySelector(".js-home__greetingJob");
+        const words = ["Frontend Developer", "Web Developer"];
+
+        let wordIndex = 0;
+        let charIndex = 0;
+        let isDeleting = false;
+
+        const typeEffect = () => {
+            const currentWord = words[wordIndex];
+            const currentChar = currentWord.substring(0, charIndex);
+            dynamicText.textContent = currentChar;
+            dynamicText.classList.add('stop-blinking')
+
+            if (!isDeleting && charIndex < currentWord.length) {
+                // If condition is true, type the next character
+                charIndex++;
+                setTimeout(typeEffect, 100);
+            } else if (isDeleting && charIndex > 0) {
+                // If condition is true, remove the previous character
+                charIndex--;
+                setTimeout(typeEffect, 80);
+            } else {
+                // If word is deleted then switch to the next word
+                isDeleting = !isDeleting;
+                dynamicText.classList.remove('stop-blinking')
+                wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+                setTimeout(typeEffect, 1200);
+            }
+
+        }
+
+        typeEffect();
+    }
+
+
 }
