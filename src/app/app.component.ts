82fe666e518cb537a2600fdd49f4f0d55abff89c
Kevin Wagner <kevin_wagner4493@web.de>
2024-03-21 10:24:07 +0100

    some small bugfixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./components/navigation/navigation.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavigationComponent]
+    imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'portfolio_blackDesign';
